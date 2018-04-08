5a15c2fe8636821f237e8f8373026a381649dece
GalaxyGamingRBX <galaxygaminghelp@gmail.com>
2018-04-08 15:04:49 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ import time
 import random
 import datetime
 
+Client = discord.Client()
+client = commands.Bot(command_prefix = ".")
+
 @client.event
 async def on_message(message):
     if message.author.id == "275050046955716609":
