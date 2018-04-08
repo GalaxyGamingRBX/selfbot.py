5c079b0879b3f46f86eea51bde74ab8859834ea0
GalaxyGamingRBX <galaxygaminghelp@gmail.com>
2018-04-08 13:30:10 -0400

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -203,7 +203,7 @@ class Selfbot(commands.Bot):
         em.description = f'{self.ws.latency * 1000:.4f} ms'
         em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
         try:
-            await ctx.send(embed=em)
+            await ctx.edit_message(embed=em)
         except discord.HTTPException:
             em_list = await embedtobox.etb(emb)
             for page in em_list:
