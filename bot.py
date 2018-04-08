194916b43c2f5d8874efb4f3bfe433dabb981119
GalaxyGamingRBX <galaxygaminghelp@gmail.com>
2018-04-08 14:57:08 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+import datetime
+
+@client.event
+async def on_message(message):
+    if message.author.id == "275050046955716609":
+       if message.content == ".ping":
+          await client.say("Ooooo")
+
+await client.login('zaneaswierz@gmail.com', 'Zane0610')
